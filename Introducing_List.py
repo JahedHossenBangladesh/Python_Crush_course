4293faf8d9fddc63ef8a87f0d08e687f684d3a55
JahedHossenBangladesh <jahedhossen4358.com>
2021-03-06 00:14:20 +0600

    this is the list sort and reverse method discussion

--- Introducing_List.py
+++ Introducing_List.py
@@ -50,5 +50,42 @@ too_expensive = 'suzuki'
 motorcycles.remove(too_expensive)
 print(motorcycles)
 
+# task ..
 
+dinner_list =["Noyon","Jony","Munna","Tohid","Tuhin","Jonak"]
+message = f'please {dinner_list[0]} come in the birthday'
+print(message)
+not_attend = dinner_list.pop()
+print(not_attend)
+dinner_list.append('Efan')
+print(dinner_list)
+pop_end = dinner_list.pop()
+print(f'please come {pop_end.upper()}')
+dinner_list.insert(0,'Sahed')
+print(dinner_list)
+dinner_list.insert(3,'Bipu')
+print(dinner_list)
+dinner_list.append('Humaiyun')
+message =f'Please come {dinner_list.pop()} in the party'
+print(message)
+message = f'please {dinner_list[0]} and {dinner_list[4]} come to the dinner'
+print(message)
+del dinner_list[0]
+print(dinner_list)
+
+dinner_list.remove('Tuhin')
+print(dinner_list)
+
+dinner_list.sort()
+print(dinner_list)
+dinner_list.sort(reverse = True)
+print('\nHerer is the orginal list:')
+print(dinner_list)
+print('\n Here is the sorted list:')
+print(sorted(dinner_list))
+dinner_list.reverse()
+print(f'the reverse is not working {dinner_list} ')
+
+invited_person = len(dinner_list)
+print(f'the invited person is {invited_person}')
 
