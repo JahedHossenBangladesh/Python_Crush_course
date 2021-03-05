b1d9e9690c3c19626644859c6d5de42a4bccce82
JahedHossenBangladesh <jahedhossen4358.com>
2021-03-05 19:40:25 +0600

    This is all about String and the some methods

--- VariableAndSimpleDataType.py
+++ VariableAndSimpleDataType.py
@@ -4,5 +4,35 @@ message = "hello python world"
 print(message)
 message = "hello python crush course "
 print(message)
-message = "the name error"
-print(mesage)
+# message = "the name error"
+# print(mesage)
+
+name = 'ada lovelace'
+print(name.title())
+# in the title method every word will be capital letter
+print(name.lower())
+print(name.upper())
+
+# Using variables in strings
+first_name = "Jahed"
+last_name = "hossen"
+full_name = f"{first_name} {last_name}"
+# f" " is call the f-strings
+print(full_name)
+
+print(f"Hello ,{full_name.title()}!")
+
+message = f"Hello, {full_name.upper()}! Jebon"
+print(message)
+# \t is for white space
+print("\tPython")
+#\t is for white space and \n is for new line
+print('languages:\n\tPython\n\tC\n\tJavaScript')
+favorite_language ="  Python"
+favorite_language = favorite_language.rstrip()
+#rstrip method keep the white space in string
+print(favorite_language)
+bad_language = ' Ruby '
+bad_language = bad_language.lstrip()
+print(bad_language)
+
