5636f7876212ec8b04f8223146e9d113df29b38f
JahedHossenBangladesh <jahedhossen4358.com>
2021-03-05 20:37:39 +0600

    this is for integer float and the zen of python

--- VariableAndSimpleDataType.py
+++ VariableAndSimpleDataType.py
@@ -65,3 +65,34 @@ print(message)
 
 the_Quote = f'   {famouse_person} once said, "A person who never made a    \nmistake never tried anything new"'
 print(the_Quote)
+
+name="jahed h"
+name = name.lstrip()
+print(name)
+name =".Jahed"
+name = name.rstrip()
+print(name)
+print(2+2)
+first = 2
+second =2.5
+third =3
+print("the integer number is ",first + third )
+print("the float number is {second}")
+underscope_with_number = 20_00_000
+print(underscope_with_number)
+x,y,z = 10,30,20
+print(y)
+
+# Constants
+MAX_NUM =5000
+print(MAX_NUM)
+
+# The Zen of Python
+# The Zen of Python, by Tim Peters Beautiful is better than ugly. 
+quot = "Simple is better than complex. "
+quot2 ="Complex is better than complicated." 
+quot3 = "Readability counts."
+quot4 =  "There should be one-- and preferably only one --obvious way to do it."
+quot5 = " Now is better than never. You could spend the rest of your life learning all the intricacies of Python and of programming in general, but then you’d never complete any proj-ects. Don’t try to write perfect code; write code that works, and then decide whether to improve your code for that project or move on to something new.As you continue to the next chapter and start digging into more involved topics, try to keep this philosophy of simplicity and clarity in mind. Experienced programmers will respect your code more and will be happy to give you feedback and collaborate with you on interesting projects."
+
+print(quot5)
