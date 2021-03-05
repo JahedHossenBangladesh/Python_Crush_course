e9f4c2a505dd5b3aaabc266079efecc4c8d70271
JahedHossenBangladesh <jahedhossen4358.com>
2021-03-05 20:52:27 +0600

    This is all about list and its index and how to call it in f-string. i make mistake in f-String

---
+++ Introducing_List.py
@@ -0,0 +1,25 @@
+# This is capter 3
+
+bicycles = ["trek","cannondale",'redline','specialize']
+print(bicycles)
+
+print(bicycles[1])
+
+print(bicycles[0].upper())
+print(bicycles[-1])
+print(bicycles[-2].title())
+
+message = f"My first bicycles is {bicycles[-3].lower()}"
+print(message)
+
+
+friends =['Tuhin','Shahed','Tohid','Munna']
+print(friends[3].upper())
+message = "Hello"
+message2 = f"hi and {message} {friends[0].upper()}"
+
+print(message2)
+
+myVachel =['car','Hunda','neno']
+message = f"I would like to take {myVachel[2]}"
+print(message)
