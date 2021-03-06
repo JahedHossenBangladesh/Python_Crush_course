b22a0c18da5ca6542fbb5d797e43bcbc4b9e6bbd
JahedHossenBangladesh <jahedhossen4358.com>
2021-03-06 20:13:12 +0600

    this is all about tuple

--- working_with_list.py
+++ working_with_list.py
@@ -10,51 +10,64 @@ for magicians in magicians:
  print(magicians)
 
 
+
 pizza =['megdonal','donut','kfc','cheap']
 for pizza in pizza:
    print(f'i like {pizza.upper()} pizza')
    print('how much you like pizza?')
+
 
 animal = ['Monky','elephan','dog','Tiger']
 for animal in animal:
     print(f'The animal name is {animal.upper()}')
     print(f'{animal.upper()} is a grate pet')
 
+
 for value in range(1,5):
     print(value)
+
 
 number = list(range(2,10))
 print(number)
 
+
 even_number =list(range(2,10,2))
 print(even_number)
+
 
 squares =[]
 for value in range(1,11):
   squares.append(value**2)
 print(squares)
 
+
 min_digit = min(squares)
 print(min_digit)
+
 
 max_digit = max(squares)
 print(max_digit)
 print(sum(squares))
 
+
 squares = [value**2 for value in range(1,11)]
 print(squares)
+
 
 list_data = []
 for value in range(1,20):
     print(value)
 # for vlaue in range(1,10000):
 #     print(vlaue)
+
 for value in range(3,30):
     print(value)
+
 
 for value in range(1,10):
     list_data.append(value**3)
     print(list_data)
+
 
 print(list_data[0:3])
 print(list_data[1:3])
@@ -62,22 +75,28 @@ print(list_data[:4])
 print(list_data[2:])
 print(list_data[-3:])
 
+
 player = ['i','you','they','ours']
 for player in player[:3]:
     print(player.title())
+
 
 my_foods =['pizza','falafel','carrot cake']
 friend_foods = my_foods[:]
 print(my_foods)
 print(friend_foods)
 
+
 my_foods.append('cannoli')
 friend_foods.append('ice cream')
+
 
 print(my_foods)
 print(friend_foods)
 
+
 # Task 
+
 message =["I","love",'you','so','much']
 print(message[:3])
 print(message[3:])
