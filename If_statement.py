6e08deaf8f6db2cd527a2865e09305ed2dd217ba
JahedHossenBangladesh <jahedhossen4358.com>
2021-03-08 12:08:20 +0600

    this is if statement dask

--- If_statement.py
+++ If_statement.py
@@ -93,3 +93,130 @@ elif 'sahed' in banned_users:
     print('shahed is in the list')
 
 print('\nFinishing the line')
+
+# Task 5-3
+alien_color = ['green','yellow','red']
+
+if alien_color == 'green':
+    print('The player earned 5 points')
+else:
+    print('')
+
+# Task 5-4
+if alien_color != 'green':
+    print('The player earn 10')
+else:
+    print('the color is not match')
+
+# Task 5-5
+alien_color.append('perpal')
+
+if alien_color == 'green':
+    print('Player earned 5 points')
+elif alien_color == 'yellow':
+    print('Player earned 10 points')
+elif alien_color == 'red':
+    print('player earned 15 points')
+
+# Task 5-6
+age = 20
+
+if age < 2:
+    print('person is a baby')
+elif age == 2 and age < 4:
+    print('The person is a toddler')
+elif age == 4 and age <13:
+    print('The person is kid')
+elif age == 13 and age <20:
+    print('The person is teenager')
+elif age == 20 and age < 65:
+    print('The person is an adult')
+elif age >= 65 :
+    print('The person is elder')
+
+# 5-7
+
+fruit = ['apple','banana','pinapple','jack fruit']
+
+if fruit =='apple':
+    print('I like apple')
+elif fruit =='banana':
+    print('I like banana')
+elif fruit =='pinapple':
+    print('This is pinapple')
+elif fruit =='jack fruit':
+    print('this is jack fruit')
+else:
+    print('the code is not working')
+
+for i in fruit:
+    if i == 'apple':
+        print('this is apple')
+    elif i == 'banana':
+          print('this is banana')
+    else:
+        print('this is nothing')
+
+my_fruint = ['apple','banana','Jamrul']
+
+if my_fruint in fruit:
+    print('yes , the food is in my list')
+else:
+    print('no that is not in my list')
+    
+
+if 'apple' in fruit:
+    print('yes,apple in the list')
+elif 'banana' in fruit:
+    print('yes, banana in the list')
+else:
+    print('no, it is not in the list')
+
+requested_topping =['mushrooms','green peppers','extra cheese']
+for requested_topping in requested_topping:
+    print(f'Adding {requested_topping}')
+
+print('\n Finishing line')
+
+requested_topping =['mushrooms','green peppers','extra cheese']
+for requested_topping in requested_topping:
+    if requested_topping == 'green peppers':
+        print('soory green peppers is out of stock')
+    else:
+        print(f'Adding {requested_topping}')
+    
+    print('\n the line is finished')
+
+
+requested_topping=[]
+if requested_topping:
+    for requested_topping in requested_topping:
+        print(f'adding {requested_topping}')
+        print('\n adding new line')
+else:
+    print('Are you sure you want a planin pizza?')
+
+available_toppings = ['mushrooms', 'olives', 'green peppers','pepperoni', 'pineapple', 'extra cheese']
+request = ['mushrooms', 'french fries', 'extra cheese']
+for request in available_toppings:
+    if request in available_toppings:
+        print(f'adding {request}')
+    else:
+        print(f'Sorry,we dont have {request}')
+              
+print('\n Finishing making your pizza')
+
+
+
+user = ['admin','user','buyer','seller','owner','worker']
+for user in user:
+    if user == 'admin':
+        print(f'welcome mr {user}')
+    else:
+        print(f'hello, {user} thank you for logging')
+
+users = []
+if users == []:
+    print('we need to fined some one')
+else:
+    print('this is not empty')
